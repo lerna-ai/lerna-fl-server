ad4a11b9501e379757abb6abfa15960f2716a145
Miltos Poulizos <mpoulizos@gmail.com>
2022-01-11 01:58:20 +0200

    LERMVP-6 Increase code coverage on StorageService

--- src/test/groovy/ai/lerna/flapi/api/TestApiSpec.groovy
+++ src/test/groovy/ai/lerna/flapi/api/TestApiSpec.groovy
@@ -3,18 +3,18 @@ package ai.lerna.flapi.api
 import spock.lang.Specification
 
 class TestApiSpec extends Specification {
-  TestApi testApi
+	TestApi testApi
 
-  def setup() {
-    testApi = new TestApiImpl(null, null)
-  }
+	def setup() {
+		testApi = new TestApiImpl(null, null)
+	}
 
-  def "Should return correct response"() {
-    given:
+	def "Should return correct response"() {
+		given:
 
-    when:
-      String response = testApi.index()
-    then:
-      response == "FL API"
-  }
+		when:
+			String response = testApi.index()
+		then:
+			response == "FL API"
+	}
 }
