d2a26603c8fb67ba41722f1ef42931464590d9d5
gkellaris <gkellaris@GS65Stealth9SG8b6654f3>
2022-02-16 12:34:24 -0500

    LERMVP-104 Built the initial data

--- src/main/resources/db/migration/V202112201658598__ADD_TEST_USER.sql
+++ src/main/resources/db/migration/V202112201658598__ADD_TEST_USER.sql
@@ -1,17 +1,33 @@
 INSERT INTO lerna_user (firstname, lastname, email, company)
-VALUES ('Miltos', 'Poulizos', 'miltos@lerna.ai', 'Lerna');
+VALUES ('Lerna', 'Inc.', 'info@lerna.ai', 'Lerna');
 
 INSERT INTO lerna_app (user_id, token, current_version, no_ml_tasks, no_min_users)
 VALUES (currval('lerna_user_id_seq'), 'e3785aec-e010-4e31-a822-705544e34f08', 1, 1, 2);
 
 INSERT INTO lerna_ml (app_id, model, privacy_parameters, ml_parameters, accuracy)
-VALUES (currval('lerna_app_id_seq'), 'hair color', '{"mpc": true, "epsilon": 1000}', '{"normalization": 0.1, "iterations": 10,"learningRate": 0.05, "dimensions": 5, "dataSplit": 70}', 0);
+VALUES (currval('lerna_app_id_seq'), 'LR - next app category', '{"mpc": true, "epsilon": 100}', '{"normalization": 0.1, "iterations": 10,"learningRate": 0.05, "dimensions": 56, "dataSplit": 80}', 0);
 
 INSERT INTO lerna_job (ml_id, prediction, weights, total_data_points, total_devices)
-VALUES (currval('lerna_ml_id_seq'), 'black', '{"array": "AAdKQVZBQ1BQAAAACAADSU5UAAAAAgAAAAUAAAABAAAAAQAAAAEAAAAAAAAAAQAAAGMAB0pBVkFD\r\nUFAAAAAFAAVGTE9BVAAAAAAAAAAAAAAAAAAAAAAAAAAA\r\n"}', 100, 10);
+VALUES (currval('lerna_ml_id_seq'), 'audio', '{"array": "AAdKQVZBQ1BQAAAACAADSU5UAAAAAgAAADgAAAABAAAAAQAAAAEAAAAAAAAAAQAAAGMAB0pBVkFD\r\nUFAAAAA4AAVGTE9BVL8zizO+mky2vwYgRb9GzWo/U5cVv73rwb8dMPQ/hmqUP5bK87+NgTW/Ax0A\r\nPjBXID9aHhw/UARmuxZkUD7C5lu/SSunv4ndLr+W3w0++GA9PtfrnDwvvoi/AcUIv5JAdL4SqJQ/\r\nDnF3wBOa3z3XGJU/H3gtP4rKXT/5jQg/gOySPbYIJL+Gd5s/jmUQvkocSsAzYR6/rDbnPwt9y78J\r\nbMa+oc2Lvyzf2T8UtII+4tHDQAcMor7SOlq/HxC3PbhYkD/+r5g/7mWPv1yf/j4V7j2/BNUzP6h0\r\n70AO560/I1oB\r\n"}', 0, 0);
 
 INSERT INTO lerna_job (ml_id, prediction, weights, total_data_points, total_devices)
-VALUES (currval('lerna_ml_id_seq'), 'red', '{"array": "AAdKQVZBQ1BQAAAACAADSU5UAAAAAgAAAAUAAAABAAAAAQAAAAEAAAAAAAAAAQAAAGMAB0pBVkFD\r\nUFAAAAAFAAVGTE9BVAAAAAAAAAAAAAAAAAAAAAAAAAAA\r\n"}', 100, 10);
+VALUES (currval('lerna_ml_id_seq'), 'game', '{"array": "AAdKQVZBQ1BQAAAACAADSU5UAAAAAgAAADgAAAABAAAAAQAAAAEAAAAAAAAAAQAAAGMAB0pBVkFD\r\nUFAAAAA4AAVGTE9BVL8zizO+mky2vwYgRb9GzWo/U5cVv73rwb8dMPQ/hmqUP5bK87+NgTW/Ax0A\r\nPjBXID9aHhw/UARmuxZkUD7C5lu/SSunv4ndLr+W3w0++GA9PtfrnDwvvoi/AcUIv5JAdL4SqJQ/\r\nDnF3wBOa3z3XGJU/H3gtP4rKXT/5jQg/gOySPbYIJL+Gd5s/jmUQvkocSsAzYR6/rDbnPwt9y78J\r\nbMa+oc2Lvyzf2T8UtII+4tHDQAcMor7SOlq/HxC3PbhYkD/+r5g/7mWPv1yf/j4V7j2/BNUzP6h0\r\n70AO560/I1oB\r\n"}', 0, 0);
 
 INSERT INTO lerna_job (ml_id, prediction, weights, total_data_points, total_devices)
-VALUES (currval('lerna_ml_id_seq'), 'blonde', '{"array": "AAdKQVZBQ1BQAAAACAADSU5UAAAAAgAAAAUAAAABAAAAAQAAAAEAAAAAAAAAAQAAAGMAB0pBVkFD\r\nUFAAAAAFAAVGTE9BVAAAAAAAAAAAAAAAAAAAAAAAAAAA\r\n"}', 100, 10);
+VALUES (currval('lerna_ml_id_seq'), 'image', '{"array": "AAdKQVZBQ1BQAAAACAADSU5UAAAAAgAAADgAAAABAAAAAQAAAAEAAAAAAAAAAQAAAGMAB0pBVkFD\r\nUFAAAAA4AAVGTE9BVL8zizO+mky2vwYgRb9GzWo/U5cVv73rwb8dMPQ/hmqUP5bK87+NgTW/Ax0A\r\nPjBXID9aHhw/UARmuxZkUD7C5lu/SSunv4ndLr+W3w0++GA9PtfrnDwvvoi/AcUIv5JAdL4SqJQ/\r\nDnF3wBOa3z3XGJU/H3gtP4rKXT/5jQg/gOySPbYIJL+Gd5s/jmUQvkocSsAzYR6/rDbnPwt9y78J\r\nbMa+oc2Lvyzf2T8UtII+4tHDQAcMor7SOlq/HxC3PbhYkD/+r5g/7mWPv1yf/j4V7j2/BNUzP6h0\r\n70AO560/I1oB\r\n"}', 0, 0);
+
+INSERT INTO lerna_job (ml_id, prediction, weights, total_data_points, total_devices)
+VALUES (currval('lerna_ml_id_seq'), 'maps', '{"array": "AAdKQVZBQ1BQAAAACAADSU5UAAAAAgAAADgAAAABAAAAAQAAAAEAAAAAAAAAAQAAAGMAB0pBVkFD\r\nUFAAAAA4AAVGTE9BVL8zizO+mky2vwYgRb9GzWo/U5cVv73rwb8dMPQ/hmqUP5bK87+NgTW/Ax0A\r\nPjBXID9aHhw/UARmuxZkUD7C5lu/SSunv4ndLr+W3w0++GA9PtfrnDwvvoi/AcUIv5JAdL4SqJQ/\r\nDnF3wBOa3z3XGJU/H3gtP4rKXT/5jQg/gOySPbYIJL+Gd5s/jmUQvkocSsAzYR6/rDbnPwt9y78J\r\nbMa+oc2Lvyzf2T8UtII+4tHDQAcMor7SOlq/HxC3PbhYkD/+r5g/7mWPv1yf/j4V7j2/BNUzP6h0\r\n70AO560/I1oB\r\n"}', 0, 0);
+
+INSERT INTO lerna_job (ml_id, prediction, weights, total_data_points, total_devices)
+VALUES (currval('lerna_ml_id_seq'), 'news', '{"array": "AAdKQVZBQ1BQAAAACAADSU5UAAAAAgAAADgAAAABAAAAAQAAAAEAAAAAAAAAAQAAAGMAB0pBVkFD\r\nUFAAAAA4AAVGTE9BVL8zizO+mky2vwYgRb9GzWo/U5cVv73rwb8dMPQ/hmqUP5bK87+NgTW/Ax0A\r\nPjBXID9aHhw/UARmuxZkUD7C5lu/SSunv4ndLr+W3w0++GA9PtfrnDwvvoi/AcUIv5JAdL4SqJQ/\r\nDnF3wBOa3z3XGJU/H3gtP4rKXT/5jQg/gOySPbYIJL+Gd5s/jmUQvkocSsAzYR6/rDbnPwt9y78J\r\nbMa+oc2Lvyzf2T8UtII+4tHDQAcMor7SOlq/HxC3PbhYkD/+r5g/7mWPv1yf/j4V7j2/BNUzP6h0\r\n70AO560/I1oB\r\n"}', 0, 0);
+
+INSERT INTO lerna_job (ml_id, prediction, weights, total_data_points, total_devices)
+VALUES (currval('lerna_ml_id_seq'), 'productivity', '{"array": "AAdKQVZBQ1BQAAAACAADSU5UAAAAAgAAADgAAAABAAAAAQAAAAEAAAAAAAAAAQAAAGMAB0pBVkFD\r\nUFAAAAA4AAVGTE9BVL8zizO+mky2vwYgRb9GzWo/U5cVv73rwb8dMPQ/hmqUP5bK87+NgTW/Ax0A\r\nPjBXID9aHhw/UARmuxZkUD7C5lu/SSunv4ndLr+W3w0++GA9PtfrnDwvvoi/AcUIv5JAdL4SqJQ/\r\nDnF3wBOa3z3XGJU/H3gtP4rKXT/5jQg/gOySPbYIJL+Gd5s/jmUQvkocSsAzYR6/rDbnPwt9y78J\r\nbMa+oc2Lvyzf2T8UtII+4tHDQAcMor7SOlq/HxC3PbhYkD/+r5g/7mWPv1yf/j4V7j2/BNUzP6h0\r\n70AO560/I1oB\r\n"}', 0, 0);
+
+INSERT INTO lerna_job (ml_id, prediction, weights, total_data_points, total_devices)
+VALUES (currval('lerna_ml_id_seq'), 'social', '{"array": "AAdKQVZBQ1BQAAAACAADSU5UAAAAAgAAADgAAAABAAAAAQAAAAEAAAAAAAAAAQAAAGMAB0pBVkFD\r\nUFAAAAA4AAVGTE9BVL8zizO+mky2vwYgRb9GzWo/U5cVv73rwb8dMPQ/hmqUP5bK87+NgTW/Ax0A\r\nPjBXID9aHhw/UARmuxZkUD7C5lu/SSunv4ndLr+W3w0++GA9PtfrnDwvvoi/AcUIv5JAdL4SqJQ/\r\nDnF3wBOa3z3XGJU/H3gtP4rKXT/5jQg/gOySPbYIJL+Gd5s/jmUQvkocSsAzYR6/rDbnPwt9y78J\r\nbMa+oc2Lvyzf2T8UtII+4tHDQAcMor7SOlq/HxC3PbhYkD/+r5g/7mWPv1yf/j4V7j2/BNUzP6h0\r\n70AO560/I1oB\r\n"}', 0, 0);
+
+INSERT INTO lerna_job (ml_id, prediction, weights, total_data_points, total_devices)
+VALUES (currval('lerna_ml_id_seq'), 'video', '{"array": "AAdKQVZBQ1BQAAAACAADSU5UAAAAAgAAADgAAAABAAAAAQAAAAEAAAAAAAAAAQAAAGMAB0pBVkFD\r\nUFAAAAA4AAVGTE9BVL8zizO+mky2vwYgRb9GzWo/U5cVv73rwb8dMPQ/hmqUP5bK87+NgTW/Ax0A\r\nPjBXID9aHhw/UARmuxZkUD7C5lu/SSunv4ndLr+W3w0++GA9PtfrnDwvvoi/AcUIv5JAdL4SqJQ/\r\nDnF3wBOa3z3XGJU/H3gtP4rKXT/5jQg/gOySPbYIJL+Gd5s/jmUQvkocSsAzYR6/rDbnPwt9y78J\r\nbMa+oc2Lvyzf2T8UtII+4tHDQAcMor7SOlq/HxC3PbhYkD/+r5g/7mWPv1yf/j4V7j2/BNUzP6h0\r\n70AO560/I1oB\r\n"}', 0, 0);
+
