80044c20bbf9dc5984ba7d3586351a50c1b9c916
gkellaris <gkellaris@GS65Stealth9SG8b6654f3>
2022-02-25 11:13:34 -0500

    5 min users

--- src/main/resources/db/migration/V202112201658598__ADD_TEST_USER.sql
+++ src/main/resources/db/migration/V202112201658598__ADD_TEST_USER.sql
@@ -2,7 +2,7 @@ INSERT INTO lerna_user (firstname, lastname, email, company)
 VALUES ('Lerna', 'Inc.', 'info@lerna.ai', 'Lerna');
 
 INSERT INTO lerna_app (user_id, token, current_version, no_ml_tasks, no_min_users)
-VALUES (currval('lerna_user_id_seq'), 'e3785aec-e010-4e31-a822-705544e34f08', 1, 1, 2);
+VALUES (currval('lerna_user_id_seq'), 'e3785aec-e010-4e31-a822-705544e34f08', 1, 1, 5);
 
 INSERT INTO lerna_ml (app_id, model, privacy_parameters, ml_parameters, accuracy)
 VALUES (currval('lerna_app_id_seq'), 'LR - next app category', '{"mpc": true, "epsilon": 100}', '{"normalization": 0.1, "iterations": 10,"learningRate": 0.05, "dimensions": 62, "dataSplit": 80}', 0);
